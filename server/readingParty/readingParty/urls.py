1baa8516881c5310ba2cad520a1be491232de4ce
shawn <stardust1900@hotmail.com>
2014-06-09 17:31:45 +0800

    上传文件

--- server/readingParty/readingParty/urls.py
+++ server/readingParty/readingParty/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'readingParty.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^sound/', include('sound.urls')),
 )
--- server/readingParty/sound/views.py
+++ server/readingParty/sound/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext
+from sound.forms import SoundForm
 # Create your views here.
+def list(request):
+	#return HttpResponse("Hello world")
+	return render_to_response('list.html');
+
+def toUpload(request):
+	#return HttpResponse("Hello world")
+	form = SoundForm()
+
+	return render_to_response('upload.html',{'form': form},context_instance=RequestContext(request));
+
+def upload(request):
+	#return HttpResponse("Hello world")
+	return render_to_response('list.html');
