2fbe8daaf67d82424c5a8588b17ea306eb0aa32e
shawn <stardust1900@hotmail.com>
2014-06-24 10:36:08 +0800

    修改url跳转

--- server/readingParty/sound/views.py
+++ server/readingParty/sound/views.py
@@ -1,9 +1,11 @@
+#coding=utf-8  
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
 from sound.forms import SoundForm
 from sound.models import Sound
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.conf import settings
 # Create your views here.
 def list(request):
@@ -23,20 +25,19 @@ def upload(request):
 		    newSound.save()
 	    else:
 		    return render_to_response('upload.html',{'form': form},context_instance=RequestContext(request));
-	else:
-		form = SoundForm()
-	sounds = Sound.objects.all()
-	return render_to_response('list.html',{'sounds':sounds, 'form': form},context_instance=RequestContext(request))
+	# else:
+	# 	form = SoundForm()
+	# sounds = Sound.objects.all()
+	# return render_to_response('list.html',{'sounds':sounds, 'form': form},context_instance=RequestContext(request))
+	#使用url转向 更改url到list
+	return HttpResponseRedirect(reverse('sound.views.list'))
 
 def toEdit(request,soundId):
 	s = Sound.objects.get(id=soundId)
 	data = {'':None,'memo':s.memo,'bookUrl':s.bookUrl}
 	form = SoundForm(data)
 	form.soundId = s.id
-	# form.memo = s.memo
-	# form.bookUrl = s.bookUrl
-	# form.id = s.id
-	print(form)
+
 	return render_to_response('edit.html',{'form': form},context_instance=RequestContext(request));
 
 def edit(request):
@@ -50,10 +51,13 @@ def edit(request):
 		s.save()
 	else:
 		print("get")
-	sounds = Sound.objects.all()
-	return render_to_response('list.html',{'sounds':sounds},context_instance=RequestContext(request))
+	# sounds = Sound.objects.all()
+	# return render_to_response('list.html',{'sounds':sounds},context_instance=RequestContext(request))
+	#使用url转向 更改url到list
+	return HttpResponseRedirect(reverse('sound.views.list'))
+
 
 def remove(request,soundId):
 	s = Sound.objects.get(id=soundId)
 	s.delete()
-	return  list(request)
+	return  HttpResponseRedirect(reverse('sound.views.list'))
