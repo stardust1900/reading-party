86e9fe62780275fa85f786d9e7c00822d0b35e2c
shawn <stardust1900@hotmail.com>
2014-06-25 16:18:51 +0800

    修改首页跳转

--- server/readingParty/readingParty/urls.py
+++ server/readingParty/readingParty/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^sound/', include('sound.urls')),
+    url(r'^$', RedirectView.as_view(url='/sound/list/')),
 ) 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
