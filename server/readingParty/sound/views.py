94bfd910fa046f5d312db01e0fede24444184712
shawn <stardust1900@hotmail.com>
2014-06-27 10:09:09 +0800

    增加分页

--- server/readingParty/sound/views.py
+++ server/readingParty/sound/views.py
@@ -8,9 +8,25 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+
 # Create your views here.
 def list(request):
+	limit = 10  # 每页显示的记录数
 	sounds = Sound.objects.all()
+	paginator = Paginator(sounds, limit)  # 实例化一个分页对象
+
+	page = request.GET.get('page')  # 获取页码
+	try:
+	    sounds = paginator.page(page)  # 获取某页对应的记录
+	except PageNotAnInteger:  # 如果页码不是个整数
+	    sounds = paginator.page(1)  # 取第一页的记录
+	except EmptyPage:  # 如果页码太大，没有相应的记录
+	    sounds = paginator.page(paginator.num_pages)  # 取最后一页的记录
+
 	return render_to_response('list.html',{'sounds':sounds})
 
 @login_required
